6ecc9eb4af21b7dcf2f745b1da30f1e69c0caeca
Nikan86p <67387467+Nikan86p@users.noreply.github.com>
2025-12-20 17:46:32 +0330

    Adding the details that we wrote in the class

--- Just-results/test5.py
+++ Just-results/test5.py
@@ -10,3 +10,9 @@ with open(
     f.write("NAME,AGE\n")
     for key in my_dictionary.keys():
         f.write(f"{key},{my_dictionary[key]}\n")
+
+with open ("/Users/nikan86p/Downloads/Nikan-mabani/TA-test/Just-results/final_file.csv","r") as f:
+    x=f.read()
+    print(x)
+
+
